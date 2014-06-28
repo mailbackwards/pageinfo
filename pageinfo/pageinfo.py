a319f3f95d9785684315d4de0b59825dde98a55d
Liam Andrew <landrew@mit.edu>
2014-06-27 23:38:16 -0400

    Consolidate into one function, add raw_html input option, returns raw_html

--- pageinfo/pageinfo.py
+++ pageinfo/pageinfo.py
@@ -6,30 +6,21 @@ import re
 from urlparse import urlparse
 from urlparse import urljoin
 
-def validate_url(input_str):
-    parsed_uri = urlparse( input_str )
-    if parsed_uri.scheme and parsed_uri.netloc:
-        return True
-    return False
-
-def get_html(url_or_html):
-    if validate_url(url_or_html):
-        response = requests.get(url_or_html, timeout=3)
-        if not response.status_code == 200:
-            return None
-        html = response.text
-    else:
-        html = url_or_html
-    return html
-
-def get_pageinfo(url_or_html):
-    html = get_html(url_or_html)
-    return json.dumps(get_meta(html))
-
 #get title, description, favicon, twitter card, facebook open graph data
-def get_meta(html):
+def get_meta(url=None, raw_html=None):
+    if url:
+        response = requests.get(url, timeout=3)
+        if not response.status_code == 200:
+            return json.dumps({"canonical": url, "error":"URL returned status"+str(response.status_code)})
+        raw_html = response.text
+
+        parsed_uri = urlparse( url )
+        domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    else:
+        domain = None
     
     data = {}
+    data["raw_html"] = raw_html
     data["title"] = ""
     data["canonical"] = None
     data["description"] = None
@@ -38,23 +29,25 @@ def get_meta(html):
     data["twitter"] = {}
 
     try:
-        soup = BeautifulSoup(html)
+        soup = BeautifulSoup(raw_html)
 
         #get title
         if soup.title.string:
             data["title"] = soup.title.string
         #get canonical
         canonical = soup.find("link", rel="canonical")
-        if canonical:
-            data["canonical"] = canonical['href']
+        if canonical and domain:
+            data["canonical"] = urljoin( domain, canonical["href"] )
+        elif canonical:
+            data["canonical"] = canonical["href"]
+            parsed_uri = urlparse(data["canonical"])
+            domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
         #get favicon
-        parsed_uri = urlparse( url )
         if soup.find("link", rel="shortcut icon"):
             icon_rel = soup.find("link", rel="shortcut icon")["href"]
-            icon_abs = urljoin( url, icon_rel )
+            icon_abs = urljoin( url, icon_rel ) if url else icon_rel
             data["favicon"] = icon_abs
-        else:
-            domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+        elif domain:
             data["favicon"] = domain + 'favicon.ico'
 
         #get description
@@ -70,13 +63,15 @@ def get_meta(html):
                     data["description"] = tag["content"]
 
         #get twitter card data
-        if soup.findAll('meta', attrs={'name':re.compile("^twitter")}):
-            for tag in soup.findAll('meta', attrs={'name':re.compile("^twitter")}):
-                tag_type = tag['name']
-                if 'content' in tag.attrs:
-                    data["twitter"][tag_type] = tag['content']
-                    if tag_type == "twitter:description" and data["description"] is None:
-                        data["description"] = tag["content"]
+        for attr in ('name', 'property'):
+            tags = soup.findAll('meta', attrs={attr:re.compile("^twitter")})
+            for tag in tags:
+                tag_type = tag[attr]
+                alt_attr = 'content' if 'content' in tag.attrs else 'value'
+                if alt_attr in tag.attrs:
+                    data["twitter"][tag_type] = tag[alt_attr]
+                if tag_type == "twitter:description" and data["description"] is None:
+                    data["description"] = tag[attr]
         # make sure canonical exists, use og as backup
         if not data['canonical'] or len(data['canonical']) == 0:
             if data['facebook'].has_key('og:url'):
@@ -84,6 +79,6 @@ def get_meta(html):
         if not data['canonical'] or len(data['canonical']) == 0:
             data['canonical'] = url
 
-        return data
+        return json.dumps(data)
     except HTMLParseError:
-        return {"canonical":url,"error":"Error parsing page data"}
+        return json.dumps({"canonical":url,"error":"Error parsing page data"})
